68e2c7f8a792de2e8e3224a6525c7264550cbefb
jangwoo0305 <tjwkddn970305@gmail.com>
2025-02-15 20:17:23 +0900

    add unichan

---
+++ Assets/Scripts/Managers/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.position += new Vector3(0.0f, 0.0f ,1.0f);
+        if (Input.GetKey(KeyCode.S))
+            transform.position -= new Vector3(0.0f, 0.0f, 1.0f);
+        if (Input.GetKey(KeyCode.A))
+            transform.position -= new Vector3(1.0f, 0.0f, 0.0f);
+        if (Input.GetKey(KeyCode.D))
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+    }
+}
