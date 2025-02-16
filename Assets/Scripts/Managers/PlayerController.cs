a9fa0c304754c7673f652d4f70aeb08e289b6292
jangwoo0305 <tjwkddn970305@gmail.com>
2025-02-16 21:37:20 +0900

    class location

--- Assets/Scripts/Managers/PlayerController.cs
+++ Assets/Scripts/Managers/PlayerController.cs
@@ -1,23 +1,29 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    float _speed = 10.0f;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        // Local -> World : TransformDirection
+        // World -> Local : InverseTransformDirection
+
         if (Input.GetKey(KeyCode.W))
-            transform.position += new Vector3(0.0f, 0.0f ,1.0f);
+            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.S))
-            transform.position -= new Vector3(0.0f, 0.0f, 1.0f);
+            transform.Translate(Vector3.back * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.A))
-            transform.position -= new Vector3(1.0f, 0.0f, 0.0f);
+            transform.Translate(Vector3.left * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.D))
-            transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+            transform.Translate(Vector3.right * Time.deltaTime * _speed);
     }
 }
